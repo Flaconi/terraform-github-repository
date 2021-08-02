e99b1cd50bbd18c410a2dcf9a7f23ab099a3adea
Sergey Novikov <sergey.novikov@flaconi.de>
2021-08-02 17:01:36 +0200

    Fix issue with github provider requirements

--- versions.tf
+++ versions.tf
@@ -1,8 +1,10 @@
 
 terraform {
   required_version = ">= 0.12"
-}
-
-provider "github" {
-  version = "~> 4"
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4"
+    }
+  }
 }
