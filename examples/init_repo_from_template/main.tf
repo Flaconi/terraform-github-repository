3d7e2459943f3702d2526e85641fd23511e0ed08
Sergey Novikov <sergey.novikov@flaconi.de>
2022-02-10 13:25:56 +0100

    Add example for repository templating

---
+++ examples/init_repo_from_template/main.tf
@@ -0,0 +1,59 @@
+variable "github_token" {
+  type    = string
+  default = ""
+}
+
+variable "github_owner" {
+  type    = string
+  default = ""
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
+data "github_user" "current" {
+  username = ""
+}
+
+# This will create `terraform-example-test-template` repository
+module "template" {
+  source = "../../"
+
+  namespace   = "terraform"
+  tenant      = "example"
+  name        = "test-template"
+  description = "Terraform module example github template repository"
+
+  visibility  = "private"
+  is_template = true
+
+  default_branch_protection_enabled = false
+}
+
+# This will create `terraform-example-test-clone` repository using template `terraform-example-test-template`
+module "clone" {
+  source = "../../"
+
+  namespace   = "terraform"
+  tenant      = "example"
+  name        = "test-clone"
+  description = "Terraform module example github clone repository"
+
+  visibility = "private"
+  template = {
+    owner      = var.github_owner
+    repository = module.template.repository.name
+  }
+
+  default_branch_protection_enabled = false
+}
+
+output "template" {
+  value = module.template.repository
+}
+
+output "clone" {
+  value = module.clone.repository
+}
